92972115b142ca7deff6e3d6c25d84fc7778d25e
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-13 00:29:26 +0530

    fix DropdownMenuTrigger asChild type error

--- src/app/(dashboard)/invoices/page.tsx
+++ src/app/(dashboard)/invoices/page.tsx
@@ -101,11 +101,9 @@ export default function InvoicesPage() {
                   </TableCell>
                   <TableCell className="text-right">
                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
+                      <DropdownMenuTrigger className="flex h-8 w-8 items-center justify-center rounded-md hover:bg-slate-100 dark:hover:bg-slate-800 outline-none">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
                       </DropdownMenuTrigger>
                       <DropdownMenuContent align="end">
                         <DropdownMenuItem>View Details</DropdownMenuItem>
